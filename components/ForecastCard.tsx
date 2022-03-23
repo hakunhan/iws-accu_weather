7c9aedd07f8883750f7f5d42d9e27f8c4a0d73a1
Hoang Hieu <hakunhan@gmail.com>
2022-03-23 14:54:46 +0700

    update css

--- components/ForecastCard.tsx
+++ components/ForecastCard.tsx
@@ -7,10 +7,9 @@ type ForecastCardProps = {
 }
 
 export function ForecastCard(props: ForecastCardProps){
-  console.log(props)
   return (
     <>
-      <div className="card w-40 bg-base-100 shadow-sm">
+      <div className="card w-36 bg-base-100 shadow-md">
         <WeatherIcon iconId={props.dailyForecast.weather[0].icon}/>
         <div className="card-body p-2 items-center text-center">
           <h2 className="card-title">{DateTime.fromSeconds(props.dailyForecast.dt, {zone: 'utc'}).toFormat('EEE')}</h2>
